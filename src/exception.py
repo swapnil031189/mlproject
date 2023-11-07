e55af32bedacf099a15c8a6e93ec48ca55dcf9c1
swapnil031189 <ssamale.electrical@gmail.com>
2023-11-07 20:28:22 +0530

    Correction in exception.py error_message_detail

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
 from src.logger import logging
 
-def erorr_message_details(error,error_detail:sys):
+def erorr_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()  #info about exception- like which file, line number all those details will be stored in exc_tb
     file_name = exc_tb.tb_frame.f_code.co_filename # custom document handling documentation
     error_message = "Error occured in Python Script name [{0}, line number [{1}]] and error message is [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
@@ -11,7 +11,7 @@ def erorr_message_details(error,error_detail:sys):
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = erorr_message_details(error_message,error_detail=error_detail)
+        self.error_message = erorr_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
@@ -23,5 +23,5 @@ class CustomException(Exception):
     
 #     except Exception as e:
 #         logging.info("Divide by Zero Error")
-#         raise CoustomException(e,sys)
+#         raise CustomException(e,sys)
     
